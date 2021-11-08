ca9dc815ad0ea33203f6b269a452d7d683fbc63e
Yong Jiun Lew <yongjiun.lew@gmail.com>
2021-11-09 00:33:47 +0800

    testing

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php include_once("index.html"); ?>
+<?php 
+include_once("index.html"); 
+header("Content-Security-Policy: base-uri 'self'; connect-src 'self' https://wadgroup31-e83d0-default-rtdb.asia-southeast1.firebasedatabase.app; default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src 'self'; img-src 'self' https://www.gstatic.com; manifest-src 'self'; media-src 'self'; object-src 'none'; report-uri https://618940ea57efdeabc05d1069.endpoint.csper.io/; script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js; style-src 'report-sample' 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com https://www.gstatic.com; worker-src 'none';");
+
+
+
+
+
+?>
+
