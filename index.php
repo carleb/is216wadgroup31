4d90c983dcce887d74c76a3312c34339955c5de9
Yong Jiun Lew <yongjiun.lew@gmail.com>
2021-11-09 11:14:58 +0800

    testing

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 include_once "index.html";
-header("Content-Security-Policy: base-uri 'self'; connect-src 'self' https://wadgroup31-e83d0-default-rtdb.asia-southeast1.firebasedatabase.app; default-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src 'self'; img-src 'self' https://www.gstatic.com; manifest-src 'self'; media-src 'self'; object-src 'none'; report-uri https://618940ea57efdeabc05d1069.endpoint.csper.io/; script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js; style-src 'report-sample' 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com https://www.gstatic.com; worker-src 'none';");
+header("base-uri 'self';
+connect-src 'self' https://wadgroup31-e83d0-default-rtdb.asia-southeast1.firebasedatabase.app;
+default-src 'self';
+font-src 'self' https://fonts.gstatic.com;
+frame-src 'self';
+img-src 'self' https://www.gstatic.com;
+manifest-src 'self';
+media-src 'self';
+object-src 'none';
+report-uri https://6189e6b457efdeabc05d1158.endpoint.csper.io/;
+script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js; style-src 'report-sample' 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com https://www.gstatic.com; worker-src 'none';");
