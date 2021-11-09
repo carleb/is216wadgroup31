4b3eeed2b3cab3405d38c31021216bdb51a7a9aa
Yong Jiun Lew <yongjiun.lew@gmail.com>
2021-11-09 11:20:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,4 +10,7 @@ manifest-src 'self';
 media-src 'self';
 object-src 'none';
 report-uri https://6189e6b457efdeabc05d1158.endpoint.csper.io/;
-script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js; style-src 'report-sample' 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com https://www.gstatic.com; worker-src 'none';");
+script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js;
+style-src 'report-sample' 'self' https://fonts.googleapis.com https://stackpath.bootstrapcdn.com https://www.gstatic.com;
+worker-src 'none';
+style-src-elem 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css;");
