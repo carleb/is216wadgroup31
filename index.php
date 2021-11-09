ff675ae7a548b8f8257491af04fe2802d982db33
Yong Jiun Lew <yongjiun.lew@gmail.com>
2021-11-09 11:26:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,4 +12,9 @@ object-src 'none';
 report-uri https://6189e6b457efdeabc05d1158.endpoint.csper.io/;
 script-src 'report-sample' 'self' https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js;
 style-src 'report-sample' 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css https://fonts.googleapis.com https://stackpath.bootstrapcdn.com https://www.gstatic.com;
-worker-src 'none';");
+worker-src 'none';
+style-src 'self' 'sha256-eQVORhjny5bZw7SnSrUDnI4H0GV+DIG7n7UMrohWnps=';
+
+
+
+");
